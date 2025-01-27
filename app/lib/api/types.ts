c3d5e67f3b3d9f674c7cee292eed4913d08020d3
josephKwarteng2 <josephkwarteng244@gmail.com>
2025-01-27 23:27:20 +0000

    chore: define types for API client configuration and HTTP methods

---
+++ app/lib/api/types.ts
@@ -0,0 +1,12 @@
+export type HttpMethod = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+
+export interface APIClientConfig {
+  baseURL: string;
+  defaultHeaders?: HeadersInit;
+}
+
+export interface LoginResponse {
+  email: string;
+  password: string;
+  token: string;
+}
