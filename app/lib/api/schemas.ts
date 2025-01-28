c51241bb55388e8d47c5e5eaca7433e98e89941c
josephKwarteng2 <josephkwarteng244@gmail.com>
2025-01-28 14:29:22 +0000

    chore(auth): add a required validator to zod schema

--- app/lib/api/schemas.ts
+++ app/lib/api/schemas.ts
@@ -2,8 +2,14 @@ import { z } from "zod";
 import { TOAST_MSGS } from "../../constants/constants";
 
 export const loginSchema = z.object({
-  email: z.string().email(TOAST_MSGS.INVALID_EMAIL),
-  password: z.string().min(6, TOAST_MSGS.PASSWORD_LENGTH),
+  email: z
+    .string()
+    .email(TOAST_MSGS.INVALID_EMAIL)
+    .nonempty(TOAST_MSGS.REQUIRED_FIELD),
+  password: z
+    .string()
+    .min(6, TOAST_MSGS.PASSWORD_LENGTH)
+    .nonempty(TOAST_MSGS.REQUIRED_FIELD),
   // rememberMe: z.boolean(),
 });
 
@@ -17,6 +23,10 @@ export const signupSchema = z
     message: TOAST_MSGS.PASSWORD_MISMATCH,
     path: ["confirmPassword"],
   });
+
+export const forgotPasswordSchema = z.object({
+  email: z.string().email(TOAST_MSGS.INVALID_EMAIL),
+});
 
 export const ErrorResponseSchema = z.object({
   message: z.string().optional(),
