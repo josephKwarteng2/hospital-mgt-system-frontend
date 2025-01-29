85179279a836257ff5bde62da961923269c560a2
josephKwarteng2 <josephkwarteng244@gmail.com>
2025-01-29 14:36:23 +0000

    chore: update constants file

--- app/constants/constants.ts
+++ app/constants/constants.ts
@@ -3,11 +3,22 @@ export const TOAST_MSGS = {
   SIGNUP_SUCCESS: "Your account has been created successfully.",
   LOGOUT_SUCCESS: "You have successfully logged out.",
   GENERIC_ERROR: "An unexpected error occurred. Please try again.",
-  REQUIRED_FIELD: "This field is required.",
-  INVALID_EMAIL: "Please enter a valid email address.",
   PASSWORD_LENGTH: "Your password must be at least 6 characters long.",
   PASSWORD_MISMATCH: "The passwords you entered do not match.",
   OTP_SENT: "A one-time password has been sent to your email address.",
+  OTP_VERIFIED: "Your one-time password has been verified.",
+};
+
+export const VALIDATION_ERRORS = {
+  REQUIRED_FIELD: "This field is required.",
+  INVALID_EMAIL: "Please enter a valid email address.",
+  PASSWORD_LENGTH: "Your password must be at least 6 characters long.",
+  PASSWORD_UPPERCASE:
+    "Your password must contain at least one uppercase letter.",
+  PASSWORD_NUMBER: "Your password must contain at least one number.",
+  PASSWORD_MISMATCH: "The passwords you entered do not match.",
+  INVALID_OTP: "Please enter a valid 6-digit one-time password.",
+  EMAIL_MAX_LENGTH: "Email address must be less than 100 characters.",
 };
 
 export const HTTP_STATUS = {
