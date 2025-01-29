2f36b5652233fc96f5228aa21b79fb0c440f14a8
josephKwarteng2 <josephkwarteng244@gmail.com>
2025-01-29 14:37:41 +0000

    chore: add user specific pages

---
+++ app/(patient)/patient-dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function PatientDashboard() {
+  return (
+    <div>
+      <header className="">
+        <h1 className="text-3xl font-semibold mb-0.5 p-6">Patient Dashboard</h1>
+      </header>
+    </div>
+  );
+}
