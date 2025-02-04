2ebb96f8c419c8bc2a19da002004257d925d360a
josephKwarteng2 <josephkwarteng244@gmail.com>
2025-02-04 23:09:50 +0000

    feat: add routeLinks for navigations

---
+++ app/constants/routeLinks.ts
@@ -0,0 +1,38 @@
+export const adminLinks = [
+  { name: "Dashboard", href: "/admin/dashboard", icon: "/dashboard.svg" },
+  { name: "Doctors", href: "/admin/doctors", icon: "/doctor.svg" },
+  { name: "Patients", href: "/admin/patients", icon: "/patient.svg" },
+  {
+    name: "Services",
+    href: "/admin/services",
+    icon: "/medical-services.svg",
+  },
+  { name: "Faqs", href: "/admin/faqs", icon: "/faq.svg" },
+  { name: "Analytics", href: "/admin/analytics", icon: "/analysis.svg" },
+];
+
+export const doctorLinks = [
+  {
+    name: "Appointments",
+    href: "/doctor/appointments",
+    icon: "/icons/appointments.svg",
+  },
+  {
+    name: "Patient Info",
+    href: "/doctor/patients",
+    icon: "/icons/patient-info.svg",
+  },
+];
+
+export const patientLinks = [
+  {
+    name: "My Appointments",
+    href: "/patient/appointments",
+    icon: "/icons/my-appointments.svg",
+  },
+  {
+    name: "My Health Records",
+    href: "/patient/health-records",
+    icon: "/icons/health-records.svg",
+  },
+];
